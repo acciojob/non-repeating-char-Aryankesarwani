e38c6b36b152b11d40bccc8cfe19fe84033dcded
Aryan Kesarwani <93967099+Aryankesarwani@users.noreply.github.com>
2025-09-25 11:44:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	freq={};
+	for(let char : str){
+		freq[char]=(freq[char] || 0) + 1;
+	}
+for(let char : str){
+	if(freq[char] === 1) return char;
 }
-const input = prompt("Enter a string");
-alert(firstNonRepeatedChar(input)); 
+return null;
+}
+// const input = prompt("Enter a string");
+// alert(firstNonRepeatedChar(input)); 
